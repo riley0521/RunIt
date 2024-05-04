b8adf1dc51287958ce084c139550e15d59fde402
Riley Farro <rileyfarro@gmail.com>
2024-05-05 00:50:33 +0800

    Create custom Theme in core-presentation-designsystem module.

---
+++ core/presentation/designsystem/src/main/java/com/rfdotech/core/presentation/designsystem/Theme.kt
@@ -0,0 +1,45 @@
+package com.rfdotech.core.presentation.designsystem
+
+import android.app.Activity
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+val DarkColorScheme = darkColorScheme(
+    primary = Green,
+    onPrimary = Black,
+    primaryContainer = Green30,
+    background = Black,
+    onBackground = White,
+    surface = DarkGray,
+    onSurface = White,
+    onSurfaceVariant = Gray,
+    secondary = White,
+    tertiary = White
+)
+
+@Composable
+fun RunItTheme(
+    content: @Composable () -> Unit
+) {
+    val colorScheme = DarkColorScheme
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = typography,
+        content = content
+    )
+}
