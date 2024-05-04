68f377b19195d06b34205a44ffdbdce9cf6ee7bd
Riley Farro <rileyfarro@gmail.com>
2024-05-05 00:04:44 +0800

    Add UiText to handle dynamic string (Can come from the server) or local string which the app can handle. Add DataErrorToText extension function to give meaning to the respective enum classes.

---
+++ core/presentation/ui/src/main/java/com/rfdotech/core/presentation/ui/DataErrorToText.kt
@@ -0,0 +1,32 @@
+package com.rfdotech.core.presentation.ui
+
+import com.rfdotech.core.domain.util.DataError
+
+fun DataError.asUiText(): UiText {
+    return when (this) {
+        DataError.Local.DISK_FULL -> UiText.StringResource(
+            R.string.error_disk_full
+        )
+        DataError.Network.REQUEST_TIMEOUT -> UiText.StringResource(
+            R.string.error_request_timeout
+        )
+        DataError.Network.TOO_MANY_REQUEST -> UiText.StringResource(
+            R.string.error_too_many_requests
+        )
+        DataError.Network.NO_INTERNET -> UiText.StringResource(
+            R.string.error_no_internet
+        )
+        DataError.Network.PAYLOAD_TOO_LARGE -> UiText.StringResource(
+            R.string.error_payload_too_large
+        )
+        DataError.Network.SERVER_ERROR -> UiText.StringResource(
+            R.string.error_server_error
+        )
+        DataError.Network.SERIALIZATION_ERROR -> UiText.StringResource(
+            R.string.error_serialization_error
+        )
+        else -> UiText.StringResource(
+            R.string.error_unknown
+        )
+    }
+}
