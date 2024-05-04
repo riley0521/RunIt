344b64c8e26383b0436e4853713d753bc315cc5b
Riley Farro <rileyfarro@gmail.com>
2024-05-04 18:34:56 +0800

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,126 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.android.libraries.mapsplatform.secrets)
+}
+
+android {
+    namespace = "com.rfdotech.runtracker"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.rfdotech.runtracker"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    // Core
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.play.services)
+
+    // Coil
+    implementation(libs.io.coil.compose)
+
+    // Google
+    api(libs.google.core)
+    implementation(libs.google.core.testing)
+    implementation(libs.google.material)
+    implementation(libs.google.maps.compose)
+    implementation(libs.google.maps.utils.ktx)
+    implementation(libs.google.play.services.location)
+    implementation(libs.google.play.services.maps)
+
+    // Desugar
+    implementation(libs.desugar.jdk.libs)
+
+    // Dependency Injection
+    implementation(libs.io.koin.core)
+    implementation(libs.io.koin.android)
+    implementation(libs.io.koin.androidx.compose)
+    implementation(libs.io.koin.androidx.workmanager)
+
+    // Ktor
+    implementation(libs.io.ktor.client.core)
+    implementation(libs.io.ktor.client.content.negotiation)
+    implementation(libs.io.ktor.client.cio)
+    implementation(libs.io.ktor.client.auth)
+    implementation(libs.io.ktor.client.logging)
+    implementation(libs.io.ktor.serialization.kotlinx.json)
+    implementation(libs.io.ktor.server.call.logging)
+
+    // Work Manager
+    implementation(libs.androidx.work.runtime.ktx)
+
+    // Timber Logger
+    implementation(libs.com.jakewharton.timber)
+
+    // Splash Screen API
+    implementation(libs.androidx.core.splashscreen)
+
+    // Crypto for Security
+    implementation(libs.androidx.security.crypto.ktx)
+
+    // MongoDB
+    implementation(libs.mongodb.bson)
+
+    // Jetpack Compose
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.androidx.navigation.compose)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+}
