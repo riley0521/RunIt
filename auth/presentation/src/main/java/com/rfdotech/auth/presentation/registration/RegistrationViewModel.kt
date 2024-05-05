a9d84545ca51bbf0ec50e9cdb8e4f8d1499e0e04
Riley Farro <rileyfarro@gmail.com>
2024-05-05 22:58:25 +0800

    Inject UserDataValidator in RegistrationViewModel and convert the email and password to flow and call validator every time the user made a change.

--- auth/presentation/src/main/java/com/rfdotech/auth/presentation/registration/RegistrationViewModel.kt
+++ auth/presentation/src/main/java/com/rfdotech/auth/presentation/registration/RegistrationViewModel.kt
@@ -1,18 +1,40 @@
-@file:Suppress("OPT_IN_USAGE_FUTURE_ERROR")
+@file:OptIn(ExperimentalFoundationApi::class)
 
 package com.rfdotech.auth.presentation.registration
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.text2.input.textAsFlow
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.rfdotech.auth.domain.UserDataValidator
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.zip
 
 class RegistrationViewModel(
-
+    private val userDataValidator: UserDataValidator
 ): ViewModel() {
 
     var state by mutableStateOf(RegistrationState())
         private set
+
+    init {
+        state.email.textAsFlow().onEach {
+            state = state.copy(
+                isEmailValid = userDataValidator.isValidEmail(it.toString())
+            )
+        }.launchIn(viewModelScope)
+
+        state.password.textAsFlow().onEach {
+            state = state.copy(
+                passwordValidationState = userDataValidator.validatePassword(it.toString())
+            )
+        }.launchIn(viewModelScope)
+    }
 
     fun onAction(action: RegistrationAction) {
         when (action) {
