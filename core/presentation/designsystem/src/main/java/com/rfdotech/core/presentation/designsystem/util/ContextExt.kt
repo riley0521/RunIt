6609e5815b75d43fd449fb6af8c3222326b56cbb
Riley Farro <rileyfarro@gmail.com>
2024-05-07 15:13:43 +0800

    Created Context Extension to show toast.

---
+++ core/presentation/designsystem/src/main/java/com/rfdotech/core/presentation/designsystem/util/ContextExt.kt
@@ -0,0 +1,13 @@
+package com.rfdotech.core.presentation.designsystem.util
+
+import android.content.Context
+import android.widget.Toast
+import androidx.annotation.StringRes
+
+fun Context.showToastStr(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
+
+fun Context.showToastRes(@StringRes resId: Int) {
+    Toast.makeText(this, resId, Toast.LENGTH_LONG).show()
+}
