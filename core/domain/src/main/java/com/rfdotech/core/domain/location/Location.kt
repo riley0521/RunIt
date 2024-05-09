7ea6aa74a94666ab212e696ce94de30c47de0bda
Riley Farro <rileyfarro@gmail.com>
2024-05-09 15:47:33 +0800

    Add class in core-domain for different Location types. Add RunDataFormatters.kt in core-presentation for formatting Duration class from Kotlin and other relevant run data.

---
+++ core/domain/src/main/java/com/rfdotech/core/domain/location/Location.kt
@@ -0,0 +1,6 @@
+package com.rfdotech.core.domain.location
+
+data class Location(
+    val latitude: Double,
+    val longitude: Double
+)
---
+++ core/domain/src/main/java/com/rfdotech/core/domain/location/LocationWithAltitude.kt
@@ -0,0 +1,6 @@
+package com.rfdotech.core.domain.location
+
+data class LocationWithAltitude(
+    val location: Location,
+    val altitude: Double
+)
