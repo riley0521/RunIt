7f344db8cf185011d4b355d5f9452ce8659ab8c6
Riley Farro <rileyfarro@gmail.com>
2024-05-10 23:55:13 +0800

    Complete set up for RunningTracker which is the core logic of our app.

--- run/domain/src/main/java/com/rfdotech/run/domain/RunningTracker.kt
+++ run/domain/src/main/java/com/rfdotech/run/domain/RunningTracker.kt
@@ -1,13 +1,24 @@
 package com.rfdotech.run.domain
 
+import com.rfdotech.core.domain.Timer
+import com.rfdotech.core.domain.location.LocationTimestamp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combineTransform
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.zip
+import kotlin.math.roundToInt
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class RunningTracker(
@@ -19,7 +30,14 @@ class RunningTracker(
         private const val OBSERVE_LOCATION_INTERVAL = 1000L
     }
 
+    private val _runData = MutableStateFlow(RunData())
+    val runData = _runData.asStateFlow()
+
+    private val isTracking = MutableStateFlow(false)
     private val isObservingLocation = MutableStateFlow(false)
+
+    private val _elapsedTime = MutableStateFlow(Duration.ZERO)
+    val elapsedTime = _elapsedTime.asStateFlow()
 
     val currentLocation = isObservingLocation
         .flatMapLatest {
@@ -29,6 +47,61 @@ class RunningTracker(
         }
         .stateIn(applicationScope, SharingStarted.Lazily, null)
 
+    init {
+        isTracking
+            .flatMapLatest {
+                if (it) {
+                    Timer.timeAndEmit()
+                } else flowOf()
+            }.onEach { value ->
+                _elapsedTime.update { it + value }
+            }
+            .launchIn(applicationScope)
+
+        currentLocation
+            .filterNotNull()
+            .combineTransform(isTracking) { location, isTracking ->
+                if (isTracking) {
+                    emit(location)
+                }
+            }.zip(_elapsedTime) { location, elapsedTime ->
+                LocationTimestamp(
+                    location = location,
+                    durationTimestamp = elapsedTime
+                )
+            }.onEach { location ->
+                val currentLocations = runData.value.locations
+                val lastLocationsList = if (currentLocations.isNotEmpty()) {
+                    currentLocations.last() + location
+                } else {
+                    listOf(location)
+                }
+                val newLocationsList = currentLocations.replaceLast(lastLocationsList)
+                val distanceMeters = LocationDataCalculator.getTotalDistanceInMeters(newLocationsList)
+                val distanceKm = distanceMeters / 1000.0
+                val currentDuration = location.durationTimestamp
+
+                val avgSecondsPerKm = if (distanceKm == 0.0) {
+                    0
+                } else {
+                    (currentDuration.inWholeSeconds / distanceKm).roundToInt()
+                }
+
+                _runData.update {
+                    RunData(
+                        distanceMeters = distanceMeters,
+                        pace = avgSecondsPerKm.seconds,
+                        locations = newLocationsList
+                    )
+                }
+            }
+            .launchIn(applicationScope)
+    }
+
+    fun setIsTracking(isTracking: Boolean) {
+        this.isTracking.update { isTracking }
+    }
+
     fun startObservingLocation() {
         isObservingLocation.update { true }
     }
@@ -36,4 +109,11 @@ class RunningTracker(
     fun stopObservingLocation() {
         isObservingLocation.update { false }
     }
+}
+
+private fun <T> List<List<T>>.replaceLast(replacement: List<T>): List<List<T>> {
+    if (this.isEmpty()) {
+        return listOf(replacement)
+    }
+    return this.dropLast(1) + listOf(replacement)
 }
